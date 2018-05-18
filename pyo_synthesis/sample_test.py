848ee8af935e43d36f9a8cd562da83e153128b62
Morgan Visnesky <morganticss@gmail.com>
2018-05-18 16:13:13 -0400

    linked soundcloud links to tracks 8 and 10

--- pyo_synthesis/sample_test.py
+++ pyo_synthesis/sample_test.py
@@ -1,6 +1,12 @@
-from pyo import *
+'''from pyo import *
 
 s = Server().boot()
 s.start()
 sf = SfMarkerLooper("sample.aif", speed=[.999,1], mul=.3).out()
+s.gui(locals())'''
+
+from pyo import *
+s = Server().boot()
+s.start()
+sf = SfPlayer("sample.aif", speed=[.999, 1], loop=True, mul=.3).out()
 s.gui(locals())
